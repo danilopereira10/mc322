e489d0cfdf18e5665e9c52eff97a3a23587fc33e
danilo.pereira <danilopereira_b2@hotmail.com.br>
2020-07-22 12:05:57 -0300

    feat: emptySquare

--- src/map/Map.java
+++ src/map/Map.java
@@ -13,19 +13,31 @@ public class Map {
 	}
 	
 	public void moveUp(Movable movable) {
+		int originalX = movable.getX();
+		int originalY = movable.getY();
 		this.matrix[movable.getY() - 1][movable.getX()] = movable;
+		matrix[originalY][originalX] = new EmptySquare();
 	}
 	
 	public void moveLeft(Movable movable) {
+		int originalX = movable.getX();
+		int originalY = movable.getY();
 		this.matrix[movable.getY()][movable.getX() - 1] = movable;
+		matrix[originalY][originalX] = new EmptySquare();
 	}
 	
 	public void moveRight(Movable movable) {
+		int originalX = movable.getX();
+		int originalY = movable.getY();
 		this.matrix[movable.getY()][movable.getX() + 1] = movable;
+		matrix[originalY][originalX] = new EmptySquare();
 	}
 	
 	public void moveDown(Movable movable) {
+		int originalX = movable.getX();
+		int originalY = movable.getY();
 		this.matrix[movable.getY() + 1][movable.getX()] = movable;
+		matrix[originalY][originalX] = new EmptySquare();
 	}
 	
 	public void printMap() {
